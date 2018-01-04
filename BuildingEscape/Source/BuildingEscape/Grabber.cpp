f2440d36a769c2b6ad245e4b4810f5f8303b51cb
Seviran <schepens.arnout@gmail.com>
2018-01-04 16:41:06 +0100

    Using DrawDebugLine

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -4,6 +4,7 @@
 #include "GameFramework/Actor.h"
 #include "Math/Rotator.h"
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 #define OUT
 
@@ -41,9 +42,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	);
 
 	// TODO Log out to test
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s; Rotation: %s"),
+	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s; Rotation: %s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
+	);*/
+
+	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
+
+	// Draw a red trace in the world to visualize
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
 	);
 
 	// Get the player viewpoint
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -25,5 +25,9 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 		
+private:
+	//How far ahead of the player can we reach in cm
+	UPROPERTY(EditAnywhere)
+	float Reach = 100.f;
 	
 };
